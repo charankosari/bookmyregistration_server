23ce0553548199dda9fc0d8714f263b7145d80f6
charan kosari <shivacharankosari099@gmail.com>
2024-06-11 16:07:23 +0530

    updated some part

--- routes/hospitalRoutes.js
+++ routes/hospitalRoutes.js
@@ -1,6 +1,6 @@
 const express=require('express')
 const router=express.Router()
-const {register, verifyRegisterOtp,sendOtp,verifyOtp,getHospitalWithDoctors,getUserDetailsByBookingId,addMoreSessions,addDoctor,updatePassword,getSingleDoctorByCode,HospitalDetails,profileUpdate, getAllHospitals,getAllDoctors, deleteDoctorById,} =require("../controllers/hospitalController")
+const {register, verifyRegisterOtp,sendOtp,verifyOtp,getHospitalWithDoctors,getHosptial,getUserDetailsByBookingId,addMoreSessions,addDoctor,updatePassword,getSingleDoctorByCode,HospitalDetails,profileUpdate, getAllHospitals,getAllDoctors, deleteDoctorById,} =require("../controllers/hospitalController")
 const {isAuthorized,roleAuthorize, isAuthorizedHosp,}=require("../middleware/auth")
 
 router.route('/')
@@ -26,6 +26,7 @@ router.route('/doctor/booking/:bookingId').get( getUserDetailsByBookingId);
 
 router.route("/admin/getalldoctors").get(getAllDoctors)
 router.route("/admin/getallhospitals").get(getAllHospitals)
+router.route("/admin/getallhospitals").get(getHosptial)
 // router.route("/admin/user/:id").get(isAuthorized,roleAuthorize("admin"),getUser)
 // .put(isAuthorized,roleAuthorize("admin"),updateUserRole).delete(isAuthorized,roleAuthorize("admin"),deleteUser)
 module.exports=router
